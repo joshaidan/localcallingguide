577a2ffda55567081575039fc7d7ec05d3eb3462
Brian Jones <joshaidan@gmail.com>
2016-05-30 20:31:31 -0400

    Initial Import

---
+++ README.md
@@ -0,0 +1,47 @@
+# Ruby Wrapper for Local Call Guide
+
+This gem is a Ruby Wrapper for [Local Calling Guide](http://www.localcallingguide.com), a website for retrieving information on local calling, area codes and telephone prefixes for North America and other countries.
+
+This gem is currently in pre-release form and is subject to constant changes.
+
+## Installation
+
+Add this line to your application's Gemfile:
+
+```ruby
+gem 'localcallingguide'
+```
+
+And then execute:
+
+    $ bundle
+
+Or install it yourself as:
+
+    $ gem install localcallingguide
+
+## Usage
+
+```ruby
+require 'localcallingguide'
+require 'pp'
+
+pp LocalCallingGuide::LocalPrefix.prefixes(250, 423)
+
+```
+
+## Development
+
+After checking out the repo, run `bin/setup` to install dependencies. Then, run `rake test` to run the tests. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
+
+To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and tags, and push the `.gem` file to [rubygems.org](https://rubygems.org).
+
+## Contributing
+
+Bug reports and pull requests are welcome on GitHub at https://github.com/joshaidan/localcallingguide. This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [Contributor Covenant](http://contributor-covenant.org) code of conduct.
+
+
+## License
+
+The gem is available as open source under the terms of the [MIT License](http://opensource.org/licenses/MIT).
+
---
+++ test/localcallingguide_test.rb
@@ -0,0 +1,48 @@
+require 'test_helper'
+
+class LocalcallingguideTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::LocalCallingGuide::VERSION
+  end
+  
+  def test_local_exchange_exists
+    assert_respond_to LocalCallingGuide::LocalExchange, "prefixes", "LocalExchange class is not defined"
+  end
+  
+  def test_local_prefix_exists
+    assert_respond_to LocalCallingGuide::LocalPrefix, "prefixes", "LocalPrefix does not respond to prefixes"
+  end
+  
+  def test_prefix_exists
+    assert_respond_to LocalCallingGuide::Prefix, "npanxx", "Prefix does not respond to npanxx"
+  end
+  
+  def test_rate_centre_exists
+    assert_respond_to LocalCallingGuide::RateCentre, "rate_centre", "Prefix does not respond to rate_centre"
+  end
+  
+  def test_distance_exists
+    assert_respond_to LocalCallingGuide::Distance, "rc_dist", "Distance does not respond to rc_dist"
+  end
+  
+  def test_local_exchange_prefix_returns_results
+    assert LocalCallingGuide::LocalExchange.prefixes('015800').size > 0, "LocalExchange.prefixes is not returning any results"
+  end
+  
+  def test_local_prefix_returns_results
+    assert LocalCallingGuide::LocalPrefix.prefixes(250, 423).size > 0, "LocalPrefix.prefixes is not returning any results"
+  end
+  
+  def test_prefix_returns_results
+    assert LocalCallingGuide::Prefix.npanxx(250, 423).size > 0, "LocalPrefix.npanxx is not returning any results"
+  end
+  
+  def test_rate_centre_returns_results
+    assert LocalCallingGuide::RateCentre.rate_centre(867).size > 0, "LocalPrefix.rate_centre is not returning any results"
+  end
+  
+  def test_distance_returns_results
+    assert LocalCallingGuide::Distance.rc_dist(416, 423, 905, 778).size > 0, "LocalPrefix.rc_dist is not returning any results"
+  end
+  
+end
