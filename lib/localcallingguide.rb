0306fafa51618a7b2c28c2ca77c1d68ce2bad58e
Brian Jones <joshaidan@gmail.com>
2016-06-09 13:36:10 -0400

    Was not using npa2 in destination parameter

--- lib/localcallingguide.rb
+++ lib/localcallingguide.rb
@@ -6,41 +6,41 @@ module LocalCallingGuide
 
   def self.get_results(url)
     data = open(url).read
-    return XmlSimple.xml_in(data, { 'ForceArray' => false })
+    XmlSimple.xml_in(data, { 'ForceArray' => false })
   end
 
   class LocalExchange
     def self.prefixes(exch, dir=1, rconly=0)
       exch_data = LocalCallingGuide.get_results("http://www.localcallingguide.com/xmllocalexch.php?exch=#{exch}&dir=#{dir}&rconly=#{rconly}")
-      return exch_data['lca-data']['prefix']
+      exch_data['lca-data']['prefix']
     end
   end
 
   class LocalPrefix
     def self.prefixes(npa, nxx, dir=1)
       prefix_data = LocalCallingGuide.get_results("http://www.localcallingguide.com/xmllocalprefix.php?npa=#{npa}&nxx=#{nxx}&dir=#{dir}")
-      return prefix_data['lca-data']['prefix']
+      prefix_data['lca-data']['prefix']
     end
   end
 
   class Prefix
     def self.npanxx(npa, nxx)
       prefix_data = LocalCallingGuide.get_results("http://www.localcallingguide.com/xmlprefix.php?npa=#{npa}&nxx=#{nxx}")
-      return prefix_data['prefixdata']
+      prefix_data['prefixdata']
     end
   end
 
   class RateCentre
     def self.rate_centre(options)
       rc_data = LocalCallingGuide.get_results("http://www.localcallingguide.com/xmlrc.php?npa=#{options[:npa]}&rc=#{options[:rate_centre]}&region=#{options[:region]}&lata=#{options[:lata]}&lir=#{options[:lir]}&ocn=#{options[:ocn]}&udate=#{options[:udate]}")
-      return rc_data['rcdata']
+      rc_data['rcdata']
     end
   end
 
   class Distance
     def self.rc_dist(npa1, nxx1, npa2, nxx2)
-      dist_data = LocalCallingGuide.get_results("http://www.localcallingguide.com/xmlrcdist.php?npa1=#{npa1}&nxx1=#{nxx1}&npa2=#{npa1}&nxx2=#{nxx2}")
-      return dist_data['rcdist']
+      dist_data = LocalCallingGuide.get_results("http://www.localcallingguide.com/xmlrcdist.php?npa1=#{npa1}&nxx1=#{nxx1}&npa2=#{npa2}&nxx2=#{nxx2}")
+      dist_data['rcdist']
     end
   end
 
