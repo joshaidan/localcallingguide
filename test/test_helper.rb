0118d76c66e835208812761eb7ade97a3a96db3a
Brian Jones <joshaidan@gmail.com>
2016-06-09 18:00:59 -0400

    Moved code climate requires to the top of file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'localcallingguide'
 
 require 'minitest/autorun'
-
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
